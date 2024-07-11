b6a6ed29fda824fa91700d7c6a64075dd49eb872
Will Lopes <devlopeswill@mtzol.com.br>
2024-07-10 23:44:27 -0300

    Create Dev2105945.sql

---
+++ Dev2105945.sql
@@ -0,0 +1,4 @@
+Desenvolvimento 2 #105945
+INSTRUÇÕES DO PROJETO
+Crie um banco de dados, adicione tabelas e determine quais são os atributos de cada uma. Em seguida, execute um trigger que se relacione com algum comando, como insert, select, delete ou update.
+Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
