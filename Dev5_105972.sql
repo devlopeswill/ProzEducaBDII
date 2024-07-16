178f13ace6ad43a2ad079de7bf5627d6707cfaf8
Will Lopes <devlopeswill@mtzol.com.br>
2024-07-16 02:09:57 -0300

    Create Dev5_105972.sql

---
+++ Dev5_105972.sql
@@ -0,0 +1,7 @@
+/*
+Desenvolvimento 5 105972
+Após uma varredura rápida no sistema de banco de dados de uma empresa de vendas, identificamos a necessidade de melhorar a segurança dessas informações. Por isso, será necessário 
+desenvolver um novo banco para armazenar os dados mais importantes, como detalhes dos clientes, valores faturados diariamente e informações sobre os produtos, além de outros. 
+Sendo assim, explique quais são os pilares da segurança de dados que devem ser seguidos para que o novo banco seja bem projetado e funcione corretamente
+Realize essa atividade no WORD ou no Bloco de Notas, suba esse arquivo para algum repositório e compartilhe o link no campo ao lado para que outros desenvolvedores possam analisá-lo. 
+*/
