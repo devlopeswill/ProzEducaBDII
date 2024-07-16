32771b251d6a2856f0733a11b54343e3b729e58d
Will Lopes <devlopeswill@mtzol.com.br>
2024-07-16 01:52:47 -0300

    Create Dev4_105962.sql

---
+++ Dev4_105962.sql
@@ -0,0 +1,3 @@
+Uma loja tem um banco de dados que contém todo o controle de vendas de produtos e de cadastro de clientes. Pensando nisso, crie uma função para somar todos os clientes que foram cadastrados na loja durante um dia.
+
+Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo. 
