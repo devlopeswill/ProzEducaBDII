4b85619da7472e36380b1576b09d4e8f18a0af73
Will Lopes <devlopeswill@mtzol.com.br>
2024-07-16 22:01:19 -0300

    Create Dev6_105978.sql

---
+++ Dev6_105978.sql
@@ -0,0 +1,3 @@
+No ano de 2022, diversas empresas sofreram tentativas de ataques cibernéticos que poderiam impactar diretamente a segurança dos dados. Portanto, é recomendado que funcionários e clientes de lojas virtuais sempre mantenham suas senhas de acesso atualizadas, alterando-as a cada três meses. Pensando nisso, cite pelo menos três métodos para melhorar essas senhas, com foco em fortalecê-las.
+
+Realize essa atividade no WORD ou no Bloco de Notas, suba esse arquivo para algum repositório e compartilhe o link no campo ao lado para que outros desenvolvedores possam analisá-lo.
